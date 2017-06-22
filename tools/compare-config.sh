e7acc48bec9cf8a4121ff417835fbf7ec71632ae
satoshinm <snmatsutake@yahoo.co.jp>
2017-06-21 22:53:29 -0700

    Add list of mod filenames, remove ATLauncher files (#23)
    * Refactor tools script set root (run from parent directory tools/)
    
    * Rename to compare-config.sh
    
    * Add list of mod filenames and script to generate the list
    
    * Remove unused ATLauncher files

---
+++ tools/compare-config.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+. tools/setroot.sh
+diff -ur config "$ROOT/config"
--- tools/setroot.sh
+++ tools/setroot.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
 ROOT=/Applications/MultiMC.app/Contents/MacOS/instances/1.7.10\ Beyond\ Reality/minecraft/
-diff -ur config "$ROOT/config"
